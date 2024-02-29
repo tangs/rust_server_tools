80442fa7fb92778a69294700d86a70167f7165e2
tangs <tangsong1988kaka@gmail.com>
2024-02-29 11:49:58 +0800

    fix sych cpp submodule.

--- sych_cpp_submodule.sh
+++ sych_cpp_submodule.sh
@@ -4,8 +4,10 @@ game="game_$1"
 
 echo $game
 
-cd ~/Documents/slots_server_rust
-git pull
-git submodule update --recursive
+# cd ~/Documents/slots_server_rust/yuchao
+cd ..
+pwd
+# git pull
+# git submodule update --recursive
 ls | grep $game | awk '{print "cd", $0"/game_library/cpp;", "git pull origin main; git submodule update --recursive;", "cd -;", "cd", $0"/game_library", "git pull; git add cpp; git commit -m \"update cpp module.\";git push;", "cd -;"}' | sh
 cd -
